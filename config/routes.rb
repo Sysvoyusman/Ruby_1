3c808f3162b5e91dab31e23413f9594ef285c2b0
usman ahmed <Sysvoy.usman@gmail.com>
2020-01-28 20:07:56 +0500

    Create rail applicaiton and setup home and about page

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'welcome/home' , to: 'welcome#home'
+  root 'welcome#home'
+  # only 'get' will change because the route is different now but the pattern is the same
+  get 'contact/about' , to: 'contact#about'
+  get 'about' , to: 'welcome#about'
+  # if write contact/about in address bar it will go to contact controller and about action method
+end
