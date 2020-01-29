a015099df0f496525ddb0d4f2fbcd629c1f6fd43
Sysvoyusman <Sysvoy.usman@gmail.com>
2020-01-29 19:04:21 +0500

    error messages are resolved with flashes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,19 @@ class ArticlesController < ApplicationController
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
+        # @article.save
+        # redirect_to article_path(@article)
+        if @article.save    #if it is going to be saved in the database
+            #do something
+            flash[:notice] = "Article was successfully created" #where we are going to display it
+            redirect_to article_path(@article)
+        else                #if it isnt goign to be saved in the database + everything else
+            render :new
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id]) # cant do it directly
     end
 
     private
