8a0878e64e4229850941465c35efa6e192adc4ba
Sysvoyusman <Sysvoy.usman@gmail.com>
2020-01-30 15:03:41 +0500

    Add partials , deletes but delete is having problem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,60 @@
 class ArticlesController < ApplicationController
+
+    def index
+        @articles = Article.all     #@articles is an instance variable
+    end
+
     def new
         @article = Article.new
     end
 
-    def create
-        #render plain: params[:article].inspect
+    def create      #new form submittion is handeled by the create action by default
+                    #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        # @article.save
-        # redirect_to article_path(@article)
-        if @article.save    #if it is going to be saved in the database
-            #do something
+                    # @article.save
+                    # redirect_to article_path(@article)
+        if @article.save       #if it is going to be saved in the database
+                    #do something
             flash[:notice] = "Article was successfully created" #where we are going to display it
             redirect_to article_path(@article)
-        else                #if it isnt goign to be saved in the database + everything else
-            render :new
+        else                   #if it isnt goign to be saved in the database + everything else
+            render :new        #show the new view
+        end
+    end
+#for edit
+    def update      #update form submittion is handeled bt the update action by default
+        @article = Article.find(params[:id])
+        if @article.update(article_params)   #if it is going to be saved in the database
+                    #do something
+            flash[:notice] = "Article was successfully updated" #where we are going to display it
+                    #if you put edit_article_path if will remain on the same page
+            redirect_to article_path(@article)
+        else                   #if it isnt goign to be saved in the database + everything else
+            render :edit       #show the edit view
         end
     end
 
+
+
+
+
+    
+
     def show
         @article = Article.find(params[:id]) # cant do it directly
+    end
+
+    def edit
+        @article = Article.find(params[:id]) # cant do it directly
+    end
+
+    def destroy
+        @article = Article.find(params[:id]) # cant do it directly
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted"
+        #redirect_to article_path(@article)
+        #redirect_to :back #only works before rail 5        
+        redirect_back(fallback_location: root_path)
     end
 
     private
