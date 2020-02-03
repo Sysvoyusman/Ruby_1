094c461e95fe6facd847eb57d0a0244439c268ba
Sysvoyusman <Sysvoy.usman@gmail.com>
2020-02-03 14:45:25 +0500

    add user validaitons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-
-end 
+    #length of the user name
+    validates :username, presence: true, 
+              uniqueness: {case_sensitive: false} ,
+              length: {minimum: 3 , maximum: 25}
+    #use regular expressions Rubular.com
+    VALID_EMAIL_REGEX= /\S+@\S+\.\S+/i
+    validates :email, presence: true, 
+              uniqueness: {case_sensitive: false} ,
+              length: {minimum: 3 , maximum: 25} , 
+              format: {with: VALID_EMAIL_REGEX}
+end  
